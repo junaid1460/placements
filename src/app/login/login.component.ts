c2a228e76a4f293c4c1becb59ff547ac300f71d4
junaid <junaid1460@gmail.com>
2018-01-24 11:23:04 +0530

    initialize component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-
+import { AngularFireAuth } from 'angularfire2/auth';
 @Component({
     templateUrl : './login.component.html',
     styleUrls : ['./login.component.css']
 })
 export class LoginComponent {
-    constructor() {
-        
+    constructor(private auth: AngularFireAuth) {
     }
 }
