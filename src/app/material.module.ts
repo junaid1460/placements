e509b8e647b46fc1890c66b0732e2ec19cea41c3
junaid <junaid1460@gmail.com>
2018-01-24 11:22:53 +0530

    import required modules

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,7 +3,17 @@ import { NgModule } from '@angular/core';
 import {
     MatToolbarModule,
     MatInputModule,
-    MatDatepickerModule
+    MatDatepickerModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule
 } from '@angular/material';
 
-export const modules = [MatToolbarModule, MatInputModule];
+export const modules = [
+    MatToolbarModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatButtonModule
+];
