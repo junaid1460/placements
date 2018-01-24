08eb6c19965609732416513f56323663cd49d113
junaid <junaid1460@gmail.com>
2018-01-24 09:05:07 +0530

    Inport angular fire and setup starter code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
+import { environment as env } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  companies: Observable<any>;
+  CompanyHandler: AngularFirestoreCollection<any>;
+  constructor(private db: AngularFirestore) {
+    this.CompanyHandler = db.collection(env.collections.companies);
+    this.companies = this.CompanyHandler.snapshotChanges().map( e => {
+      return e.map( d => ({id : d.payload.doc.id , data : d.payload.doc.data()} ));
+    });
+    document.title = env.app.name;
+  }
+  add() {
+    this.addCompany({name: 'Micro' });
+  }
+  addCompany(company: Company) {
+    this.CompanyHandler.add(company);
+  }
+  deleteCompany(docid: string) {
+    this.CompanyHandler.doc(docid).delete();
+  }
+}
+
+interface  Company {
+  name: string;
+  ctc?: string;
+  interview_date?: Date;
+  preplacement_talk_date?: Date;
+  description?: string;
+  registration_last_date?: Date;
 }
