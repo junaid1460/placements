54f70d623fc0e18a2177ef75a9f7da41c6bf378f
junaid <junaid1460@gmail.com>
2018-01-24 09:18:03 +0530

    moved vars to new file

---
+++ src/app/app.env.ts
@@ -0,0 +1,16 @@
+export const env  = {
+    firebase : {
+        apiKey: 'AIzaSyBSUDOmIOPw0GqLd_bqBzrDYH4S0jXCdqo',
+        authDomain: 'cgproject-631d0.firebaseapp.com',
+        databaseURL: 'https://cgproject-631d0.firebaseio.com',
+        projectId: 'cgproject-631d0',
+        storageBucket: 'cgproject-631d0.appspot.com',
+        messagingSenderId: '649844661943'
+      },
+      collections: {
+        companies : 'companies'
+      },
+      app: {
+        name : 'placements'
+      }
+};
