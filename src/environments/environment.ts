707a2e5a40c6d221b2f3bcbfbd33b4c782d41df7
junaid <junaid1460@gmail.com>
2018-01-24 07:40:49 +0530

    added credentials and collection informations

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,16 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: 'AIzaSyBSUDOmIOPw0GqLd_bqBzrDYH4S0jXCdqo',
+    authDomain: 'cgproject-631d0.firebaseapp.com',
+    databaseURL: 'https://cgproject-631d0.firebaseio.com',
+    projectId: 'cgproject-631d0',
+    storageBucket: 'cgproject-631d0.appspot.com',
+    messagingSenderId: '649844661943'
+  },
+  collections: {
+    companies : 'companies'
+  }
 };
