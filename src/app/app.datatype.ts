1a00b075ae37d0096e52221d51ee01a477f95c62
junaid <junaid1460@gmail.com>
2018-01-25 20:58:22 +0530

    database collection shapes

---
+++ src/app/app.datatype.ts
@@ -0,0 +1,26 @@
+export interface  Company {
+    name: string;
+    type: string;
+    registration_last_date: Date;
+    salary_offered: string;
+    sslc?: string;
+    puc?: string;
+    diploma?: string;
+    cgpa?: string;
+    dob_range_start?: Date;
+    dob_range_end?: Date;
+    max_gap_edu?: number;
+    max_active_backlogs: number;
+    extra?: string;
+}
+
+export interface User {
+    uid: string;
+    email: string;
+    cgpa: number;
+    puc: number;
+    diploma: number;
+    dob: number;
+    blocked: boolean;
+    edu_gap: number;
+}
