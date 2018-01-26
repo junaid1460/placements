824bd5e75490b87c903845fd79ed1b6a9db65ef4
junaid <junaid1460@gmail.com>
2018-01-26 22:33:29 +0530

    fixed subscribe call when user is anonymous

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import { env } from '../app.env';
 import { Router} from '@angular/router';
+import { DBService } from './db.service';
 
 @Injectable()
 export class AuthService  {
     loggedIn: Boolean = false;
-    constructor(public auth: AngularFireAuth, private router: Router) {
+    constructor(public auth: AngularFireAuth, private router: Router,
+        private db: DBService) {
         this.auth.authState.subscribe(e => {
             if (e) {
                 this.loggedIn = true;
@@ -14,9 +16,11 @@ export class AuthService  {
                 if (url === '' || url === '/login') {
                     this.router.navigate(['/news']);
                 }
+                this.db.subscribe();
             } else {
                 this.loggedIn = false;
                 this.router.navigate(['/login']);
+                this.db.unsubscribe();
             }
         });
     }
