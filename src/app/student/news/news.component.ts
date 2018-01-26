b914d15dde6ae7423fc79ac40798d172203b32cb
junaid <junaid1460@gmail.com>
2018-01-26 17:51:11 +0530

    import db service to access news array

--- src/app/student/news/news.component.ts
+++ src/app/student/news/news.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { WindowService } from '../../services/window.service';
+import { DBService } from '../../services/db.service';
 @Component({
     selector: 'app-news',
     templateUrl: './news.component.html',
     styleUrls: ['./news.component.css']
 })
 export class NewsComponent {
-    constructor(public ws: WindowService) {
+    constructor(public ws: WindowService,
+                public db: DBService) {
     }
 
 }
