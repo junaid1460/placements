ccf26133c2f928cec77bb382cf8a427c9253650d
junaid <junaid1460@gmail.com>
2018-01-26 12:35:22 +0530

    add desktop layout

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
+import { WindowService } from '../services/window.service';
 @Component({
     selector: 'app-root',
     templateUrl : './auth.component.html',
     styleUrls: ['auth.component.css']
 })
 export class AuthComponent {
-    constructor(public auth: AuthService) {
-
+    constructor(public auth: AuthService,
+                public ws: WindowService) {
     }
     signOut() {
         this.auth.auth.auth.signOut();
