929ddc5be460671d74d2acce8205107026cd9eaf
junaid <junaid1460@gmail.com>
2018-01-26 17:01:20 +0530

    set view encapsulation to None

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,10 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { WindowService } from '../services/window.service';
+
 @Component({
     selector: 'app-root',
     templateUrl : './auth.component.html',
-    styleUrls: ['auth.component.css']
+    styleUrls: ['auth.component.css'],
+    encapsulation : ViewEncapsulation.None
 })
 export class AuthComponent {
     constructor(public auth: AuthService,
