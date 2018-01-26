662b8b961775eb134f926cea55471b67da440b86
junaid <junaid1460@gmail.com>
2018-01-26 08:18:57 +0530

    setup auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import { env } from '../app.env';
+import { Router} from '@angular/router';
+
+@Injectable()
+export class AuthService  {
+    loggedIn: Boolean = false;
+    constructor(public auth: AngularFireAuth, private router: Router) {
+        this.auth.authState.subscribe(e => {
+            if (e) {
+                this.loggedIn = true;
+                this.router.navigate(['/news']);
+            } else {
+                this.loggedIn = false;
+                this.router.navigate(['/login']);
+            }
+        });
+    }
+
+}
+
