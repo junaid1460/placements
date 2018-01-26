ac953d6ef5df524b9daf187aa3e2679d78fa276d
junaid <junaid1460@gmail.com>
2018-01-26 09:31:12 +0530

    moved database code to service

--- src/app/student/company/company.component.ts
+++ src/app/student/company/company.component.ts
@@ -5,8 +5,8 @@ import { Observable } from 'rxjs/Observable';
 import { env } from '../../app.env';
 import { Company } from '../../app.datatype';
 import { MatSnackBar } from '@angular/material';
-import { Text } from '../../app.text';
 
+import { DBService } from '../../services/db.service';
 
 @Component({
   templateUrl: './company.component.html',
@@ -15,30 +15,12 @@ import { Text } from '../../app.text';
 export class CompanyComponent {
   companies: Observable<any>;
   CompanyHandler: AngularFirestoreCollection<any>;
-  constructor(private db: AngularFirestore,
-              private auth: AngularFireAuth,
-              private snck: MatSnackBar
+  constructor(
+    private db: DBService,
+    private snck: MatSnackBar
               ) {
-    this.CompanyHandler = db.collection(env.collections.companies);
-    this.companies = this.CompanyHandler.snapshotChanges().map( e => {
-      return e.map( d => ({id : d.payload.doc.id , data : d.payload.doc.data()} ));
-    });
+    this.companies = this.db.companies;
   }
 
-  register(companyId: string) {
-    const myid = this.auth.auth.currentUser.uid;
-    const data = {  registred : true};
-    this.db.collection('users')
-          .doc(myid).collection('registered').doc(companyId).set(data)
-          .then(() => {
-            this.snck.open(Text.success_register_message, null, {duration: 1000});
-          }, (err) => {
-            this.snck.open(Text.failure_register_message, null, {duration: 1000});
-          });
-  }
-
-
-  signOut() {
-    this.auth.auth.signOut();
-  }
+  register = (companyId: string) => {this.db.register(companyId); };
 }
