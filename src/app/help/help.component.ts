f8781052a8ddfb5741880ff818f511e7c3b4e2af
junaid <junaid1460@gmail.com>
2018-01-28 14:04:00 +0530

    iterable help template

--- src/app/help/help.component.ts
+++ src/app/help/help.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-
+import { WindowService } from '../services/window.service';
+import { DBService } from '../services/db.service';
 @Component({
   selector: 'app-help',
   templateUrl: './help.component.html',
   styleUrls: ['./help.component.css']
 })
-export class HelpComponent implements OnInit {
+export class HelpComponent {
 
-  constructor() { }
-
-  ngOnInit() {
-  }
+  constructor(public ws: WindowService,
+              public db: DBService) { }
 
 }
