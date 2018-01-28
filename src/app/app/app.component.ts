b01c6ee15846504b866ae796856590690e58bfd2
junaid <junaid1460@gmail.com>
2018-01-28 17:02:14 +0530

    add app-root new

---
+++ src/app/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { WindowService } from '../services/window.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(public auth: AuthService,
+    public ws: WindowService) {
+}
+
+}
