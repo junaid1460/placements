97f5ff9255b56514b5c05ede1135f71c4ec5ccde
junaid <junaid1460@gmail.com>
2018-02-20 20:11:37 +0530

    updated script

--- install.sh
+++ install.sh
@@ -24,15 +24,20 @@ sudo apt-get install --yes docker docker-compose git --allow-change-held-package
 sudo groupadd docker
 sudo usermod -aG docker "$user" #add student to docker group
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 
-source ~/.bashrc
-
-nvm install v8.0
 
 wget https://az764295.vo.msecnd.net/stable/490ef761b76b3f3b3832eff7a588aac891e5fe80/code_1.19.2-1515599945_amd64.deb -O vscode.deb
 
 sudo dpkg -i vscode.deb
 
 # install missing dependencies if exist
-sudo apt-get install -f
+sudo apt-get install -f -y
+
+sudo 
+
+
+
+sudo -H -u $user bash -c "
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+source ~/.bashrc
+nvm install v8.0"
